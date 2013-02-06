4a56d76393c1e0ae578c6da98766476dad10b470
Jp Tajakka <tajakka@gmail.com>
2013-02-06 11:15:07 +0200

    Improvements, continuous racing

--- derbyStateMachine.js
+++ derbyStateMachine.js
@@ -1,15 +1,16 @@
 var test = new derbyStateMachine("Komeetanpoika","Tähtiäinen");
 function derbyStateMachine(){
-  var defaultNames = ["Nemo", "Pongo", "Tyyne", "Silkkihieno", "Sirius", "Growltiger"];
+	var defaultNames = ["Nemo", "Pongo", "Tyyne", "Silkkihieno", "Sirius", "Growltiger"];
 	var cats = [];
 	var tracklength = 5000;
 	var coordinateUpdateInterval = 100;
-	var speedUpdateInterval = 2000;
+	var speedUpdateInterval = 4000;
+	var catsInGoal = 0;
 	function Cat(name){
 		this.name = name;
 		this.speed = 0;
 		this.coordinate = 0;
-		this.finished = false;
+		this.lastRaceTime = 0;
 	}
 	if(arguments.length != 0){
 		for(var i = 0; i < arguments.length; i++){
@@ -21,13 +22,40 @@ function derbyStateMachine(){
 			cats[i] = new Cat(defaultNames[i]);
 		}
 	}
+	function _millisecondsToStr(milliseconds){
+    // TIP: to find current time in milliseconds, use:
+    // var milliseconds_now = new Date().getTime();
+
+		var seconds = milliseconds / 1000;
+		var numyears = Math.floor(seconds / 31536000);
+		if(numyears){
+			return numyears + ' year' + ((numyears > 1) ? 's' : '');
+		}
+		var numdays = Math.floor((seconds % 31536000) / 86400);
+		if(numdays){
+			return numdays + ' day' + ((numdays > 1) ? 's' : '');
+		}
+		var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+		if(numhours){
+			return numhours + ' hour' + ((numhours > 1) ? 's' : '');
+		}
+		var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+		if(numminutes){
+			return numminutes + ' minute' + ((numminutes > 1) ? 's' : '');
+		}
+		var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+		if(numseconds){
+			return numseconds + ' second' + ((numseconds > 1) ? 's' : '');
+		}
+		return 'less then a second'; //'just now' //or other string you like;
+	}
 	function randomizeCatSpeed(cat){
 		cat.speed = Math.random()
-		console.log("Speed change:"+cat.name+":"+cat.speed+":"+cat.coordinate);
+		console.log("Speed change "+cat.name+":"+cat.speed+", Coordinate:"+cat.coordinate);
 	}
 	function setCatCoordinate(cat,coordinateUpdateInterval){
 		cat.coordinate += cat.speed * coordinateUpdateInterval;
-		console.log("Coordinate:"+cat.name+" "+cat.coordinate);
+		//console.log("Coordinate:"+cat.name+" "+cat.coordinate);
 	}
 	function raceOn(cat){
 		var previousTime = new Date();
@@ -36,17 +64,37 @@ function derbyStateMachine(){
 		var coordinate = setInterval(function(){setCatCoordinate(cat,coordinateUpdateInterval);finishedTest()},coordinateUpdateInterval);
 		function finishedTest(){
 			if(cat.coordinate > tracklength){
+				cat.lastRaceTime = (new Date()-startTime);
 				clearInterval(speed);
 				clearInterval(coordinate);
-				cat.finished = true;
-				console.log("Finished: "+ cat.name +" Time:"+(new Date()-startTime));
+				console.log("Finished: "+ cat.name +" Time:"+cat.lastRaceTime);
+				catsInGoal++;
+				if(catsInGoal == cats.length){
+					raceOff();
+				}
 			}
 		}
 	}
-	for(var i = 0; i < cats.length; i++){
-		raceOn(cats[i]);
-		console.log(cats[i].name+" started.");
+	function raceOff(){
+		for(var i = 0; i < cats.length; i++){
+			console.log(cats[i].name+" time:"+_millisecondsToStr(cats[i].lastRaceTime));
+			}
+		setTimeout(function() {raceSetup()},10000);	
 	}
+	function raceSetup(){
+		catsInGoal = 0;
+		for(i in cats) {
+			cats[i].speed = 0;
+			cats[i].coordinate = 0;
+		}
+		setTimeout(function() {
+			for(var i = 0; i < cats.length; i++){
+				raceOn(cats[i]);
+				console.log(cats[i].name+" started.")
+			};
+		},2000);
+	}
+	raceSetup();
 }
 
 	
